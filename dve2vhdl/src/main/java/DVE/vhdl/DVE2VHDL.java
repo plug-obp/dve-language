02032adebf1c7a8d05207db2583427c63f82bf70
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2020-07-09 15:32:23 +0200

    added sdve dependency

--- dve2vhdl/src/main/java/DVE/vhdl/DVE2VHDL.java
+++ dve2vhdl/src/main/java/DVE/vhdl/DVE2VHDL.java
@@ -1,8 +1,17 @@
 package DVE.vhdl;
 
 import DVE.model.System;
+import SDVE.model.util.ModelSwitch;
 
 public class DVE2VHDL {
+
+    ModelSwitch<String> sdveModelSwitch = new ModelSwitch<String>() {
+
+    };
+
+    DVE.model.util.ModelSwitch<String> dveModelSwitch = new DVE.model.util.ModelSwitch<String>() {
+
+    };
 
     public static String transform(System model) {
         return "Generated code";
